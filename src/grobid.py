974ddf90634cddc0c6c9888b605cb4e49f862771
Camille Demers <camille.demers@umontreal.ca>
2025-04-01 11:00:35 -0400

    Added to-do to grobid pdf to xml

--- src/grobid.py
+++ src/grobid.py
@@ -1,5 +1,9 @@
 from grobid_client.grobid_client import GrobidClient
 
+# To-do : traiter les sous-dossiers contenant les PDFs pour chacune des revues individuellement,
+# puis stocker les fichiers XML/TEI dans des sous-dossiers correspondant (pour l'instant, les fichiers 
+# ont été mis manuellement dans le dossier pdfs pour chaque revue une à la fois)
+
 client = GrobidClient(config_path='config.json')
 client.process(
     'processFulltextDocument', 
